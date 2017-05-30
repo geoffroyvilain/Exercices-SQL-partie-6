f8c8d72a687d211905c743a3dbcc411a3a42ec5c
Mickael Noel <mnoel.formation@novei.fr>
2017-05-30 11:39:01 +0200

    Changement du nom de la base

--- ajout.sql
+++ ajout.sql
@@ -1,4 +1,4 @@
-CREATE TABLE `webdevelopment`.`ide` (
+CREATE TABLE `webDevelopment`.`ide` (
 `id` SMALLINT UNSIGNED NOT NULL AUTO_INCREMENT,
 `name` VARCHAR(40) NOT NULL,
 `version` VARCHAR(10) NOT NULL,
@@ -7,10 +7,10 @@ PRIMARY KEY (`id`)
 )
 ENGINE=INNODB;
 
-INSERT INTO `webdevelopment`.`ide` (`name`, `version`, `date`) VALUES ('Eclipse', '3.3', '2007-06-01');
-INSERT INTO `webdevelopment`.`ide` (`name`, `version`, `date`) VALUES ('Eclipse', '3.5', '2009-06-01');
-INSERT INTO `webdevelopment`.`ide` (`name`, `version`, `date`) VALUES ('Eclipse', '3.6', '2010-06-01');
-INSERT INTO `webdevelopment`.`ide` (`name`, `version`, `date`) VALUES ('Eclipse', '3.7', '2011-06-01');
-INSERT INTO `webdevelopment`.`ide` (`name`, `version`, `date`) VALUES ('Eclipse', '4.3', '2013-06-13');
-INSERT INTO `webdevelopment`.`ide` (`name`, `version`, `date`) VALUES ('NetBeans', '7', '2011-04-01');
-INSERT INTO `webdevelopment`.`ide` (`name`, `version`, `date`) VALUES ('NetBeans', '8.2', '2016-10-03');
+INSERT INTO `webDevelopment`.`ide` (`name`, `version`, `date`) VALUES ('Eclipse', '3.3', '2007-06-01');
+INSERT INTO `webDevelopment`.`ide` (`name`, `version`, `date`) VALUES ('Eclipse', '3.5', '2009-06-01');
+INSERT INTO `webDevelopment`.`ide` (`name`, `version`, `date`) VALUES ('Eclipse', '3.6', '2010-06-01');
+INSERT INTO `webDevelopment`.`ide` (`name`, `version`, `date`) VALUES ('Eclipse', '3.7', '2011-06-01');
+INSERT INTO `webDevelopment`.`ide` (`name`, `version`, `date`) VALUES ('Eclipse', '4.3', '2013-06-13');
+INSERT INTO `webDevelopment`.`ide` (`name`, `version`, `date`) VALUES ('NetBeans', '7', '2011-04-01');
+INSERT INTO `webDevelopment`.`ide` (`name`, `version`, `date`) VALUES ('NetBeans', '8.2', '2016-10-03');
